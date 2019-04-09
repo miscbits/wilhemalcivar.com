aa744a6e83bd6848382149822d5ee5635efde7a9
miscbits <willow6786@gmail.com>
2019-04-09 19:43:46 -0400

    set up configuration script to get around angular's stupid issues with environment variables

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-career',
   templateUrl: './career.component.html',
   styleUrls: ['./career.component.css']
 })
+
 export class CareerComponent implements OnInit {
+
+  env = JSON.stringify(environment);
 
   constructor() { }
 
