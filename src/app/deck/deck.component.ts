6a8e4255dda89d897132e6fed5add5abb75e0b3d
miscbits <willow6786@gmail.com>
2019-04-15 19:14:02 -0400

    finished deck component page

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -9,16 +9,34 @@ import { DeckList } from '../models/decklist'
 })
 export class DeckComponent implements OnInit {
 
+  private signatureCards: Object;
   private deckLists: DeckList[];
   private listService: ListService;
 
   constructor(listService: ListService) {
       this.listService = listService;
+      this.signatureCards = {};
   }
 
   ngOnInit() {
-      this.listService.getDecks()
-          .subscribe(decklists => this.deckLists = decklists);
+      var decklistObserver = this.listService.getDecks();
+        
+      decklistObserver.subscribe(decklists => this.deckLists = decklists);
+      decklistObserver.subscribe(decklists => {
+        let identifiers = []
+        for (var i = decklists.length - 1; i >= 0; i--) {
+          let cardName = decklists[i].signature_card;
+          identifiers.push({name: cardName});
+        }
+
+        this.listService.getScryfallData({identifiers: identifiers})
+            .subscribe(cards => {
+                console.log(cards);
+                for(let card of cards.data) {
+                    this.signatureCards[card["name"]] = card["image_uris"]["normal"];
+                }
+            });
+      });
   }
 
 }
